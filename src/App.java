96bfba1693ae4dbb6b1782bae6b516086bee9e95
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-10-09 09:20:52 +0200

    Gjort klar del b

--- src/App.java
+++ src/App.java
@@ -15,5 +15,18 @@ public class App {
         System.out.println(tal1<tal2);
         System.out.println("tal1>tal2 är: ");
         System.out.println(tal1>tal2);
+
+        //b
+        System.out.println("Del b");
+        int[] fält = new int[3];
+        fält[0] = 4;
+        fält[1] = 5;
+        fält[2] = 2;
+        System.out.println("fält[0]>fält[2] är: ");
+        System.out.println(fält[0]>fält[2]);
+        System.out.println("fält[0]<fält[1] är: ");
+        System.out.println(fält[0]<fält[1]);
+        System.out.println("fält[0]==fält[2] är: ");
+        System.out.println(fält[0]==fält[2]);
     }
 }
