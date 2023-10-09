2fc70a12442bcd8bbfd1148ce6933d4b7c348bb3
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-10-09 09:12:25 +0200

    Gjor klar del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //a
+        System.out.println("Del a");
+        Scanner t = new Scanner(System.in);
+        System.out.println("Ange tal1: ");
+        int tal1 = t.nextInt();
+        System.out.println("Ange tal2: ");
+        int tal2 = t.nextInt();
+        System.out.println("tal1=tal2 är: ");
+        System.out.println(tal1==tal2);
+        System.out.println("tal1<tal2 är: ");
+        System.out.println(tal1<tal2);
+        System.out.println("tal1>tal2 är: ");
+        System.out.println(tal1>tal2);
     }
 }
